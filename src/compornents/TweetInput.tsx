f69927ee11c4077bcd60826bf11a8d796e01da09
廣本　祥 <shoprogramsa@gmail.com>
2021-01-29 23:59:01 +0900

    改良

--- src/compornents/TweetInput.tsx
+++ src/compornents/TweetInput.tsx
@@ -6,6 +6,8 @@ import {storage, db, auth} from "../firebase"
 import { Avatar, Button, IconButton } from '@material-ui/core';
 import firebase from "firebase/app";
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+
+
 const TweetInput: React.FC  = () => {
   const user = useSelector(selectUser);
   const [tweetImage, setTweetImage] = useState<File | null>(null);
